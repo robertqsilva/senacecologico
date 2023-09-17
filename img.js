80b8771f240d30cddd7beb754833878d350069c8
robert queiroz <robertqsilva1@gmail.com>
2023-09-17 01:41:12 -0300

    new section integration  out pages

---
+++ img.js
@@ -0,0 +1,35 @@
+let slideIndex = 0;
+let slides = document.getElementsByClassName("mySlides");
+let slideTimer;
+
+showSlides()
+
+function showSlides(num) {
+  if (slideTimer) {
+    clearTimeout(slideTimer);
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  if (num) {
+    slideIndex += num;
+  } else {
+    slideIndex++;
+  }
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  slideTimer = setTimeout(function () {
+    showSlides(1);
+  }, 10000);
+}
